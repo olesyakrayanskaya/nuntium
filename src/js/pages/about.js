377b6d797de3216066f5a080533789d9648407fe
Olesya Krayanskaya <krayanskaya_ov@mail.ru>
2023-06-23 17:51:53 +0300

    about page

--- src/js/pages/about.js
+++ src/js/pages/about.js
@@ -1,6 +1,8 @@
 import { url } from '../main';
 import '../../scss/main.scss';
 
+const aboutInner = document.querySelector('.about__inner');
+
 export default async function getData(u) {
     const response = await fetch(u);
     return response.json();
@@ -9,4 +11,12 @@ export default async function getData(u) {
 getData(`${url}about`)
     .then((data) => {
         console.log(data, 'about');
+        const aboutTitle = document.createElement('h1');
+        aboutTitle.className = 'about__title';
+        aboutTitle.innerHTML = `${data.title}`;
+        aboutInner.before(aboutTitle);
+        const article = document.createElement('article');
+        article.className = 'about__article';
+        article.innerHTML = `${data.content}`;
+        aboutInner.append(article);
     });
